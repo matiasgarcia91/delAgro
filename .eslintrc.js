0f18a09c0a1601437f21d64fcffb7d3a61680364
Matias Garcia <matiasigarcia91@gmail.com>
2018-02-15 21:00:50 -0300

    Setup basic tools

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'airbnb',
+    'plugin:jest/recommended',
+  ],
+  plugins: [
+    'import',
+    'jest',
+    'jsx-a11y',
+    'react',
+  ],
+  env: {
+    browser: true,
+    'jest/globals': true,
+  },
+  rules: {
+    'no-underscore-dangle': ['off'],
+    'class-methods-use-this': ['off'],
+    'jsx-quotes': ['off'],
+    'react/prefer-stateless-function': [0],
+    'jsx-a11y/no-autofocus': ['off'],
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: [
+        'tests/**',
+      ],
+      optionalDependencies: false,
+    }],
+  },
+};
