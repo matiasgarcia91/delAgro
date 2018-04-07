dde47551e2a75a6f5024d40eac54bc67b9505587
Matias Garcia <matiasigarcia91@gmail.com>
2018-04-07 19:36:01 -0300

    Set up android build

--- android/app/src/main/java/com/delagro/MainApplication.java
+++ android/app/src/main/java/com/delagro/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import org.reactnative.camera.RNCameraPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,7 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
             new RNFetchBlobPackage(),
             new ImagePickerPackage(),
             new ReactVideoPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new RNCameraPackage()
       );
     }
 
