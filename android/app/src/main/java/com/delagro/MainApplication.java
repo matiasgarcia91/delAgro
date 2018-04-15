c53e985dbcf1bdcdafe20be527ae4332e74a80fc
Matias Garcia <matiasigarcia91@gmail.com>
2018-04-15 19:35:36 -0300

    Upgrade android dependencies and to SDK 27

--- android/app/src/main/java/com/delagro/MainApplication.java
+++ android/app/src/main/java/com/delagro/MainApplication.java
@@ -12,7 +12,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import org.reactnative.camera.RNCameraPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -33,8 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
             new RNFetchBlobPackage(),
             new ImagePickerPackage(),
             new ReactVideoPackage(),
-            new VectorIconsPackage(),
-            new RNCameraPackage()
+            new VectorIconsPackage()
       );
     }
 
