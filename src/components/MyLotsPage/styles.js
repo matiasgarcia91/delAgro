6b949da5ab72c26f491f3566472938144b35a6cb
Matias Garcia <matiasigarcia91@gmail.com>
2018-06-09 18:09:04 -0300

    Add banner to myLots

---
+++ src/components/MyLotsPage/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../styles';
+
+export default StyleSheet.create({
+  bar: {
+    backgroundColor: colors.creamBackground,
+    flexDirection: 'row',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: colors.lightGrey,
+  },
+  text: {
+    fontSize: 22,
+  },
+});
