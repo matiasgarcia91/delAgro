b5ff5c52a5eb27341aecfff365bfc9c2d9621bf1
Matias Garcia <matiasigarcia91@gmail.com>
2018-07-20 20:09:35 -0300

    typo fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { persistStore } from 'redux-persist';
